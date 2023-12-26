4727e794f949eda2c6cbc00e9d89c3ad8a014558
Tanmay Chaudhari <hctanmay@gmail.com>
2023-12-26 12:24:35 -0500

    :sparkles: Add server code to accept data and send back

--- server.py
+++ server.py
@@ -7,3 +7,22 @@ serv_sock = socket.socket(
 )
 
 print(type(serv_sock))
+print(serv_sock.fileno())
+
+serv_sock.bind(('127.0.0.1', 6543))
+
+backlog = 10
+serv_sock.listen(backlog)
+
+client_sock, client_addr = serv_sock.accept()
+print(client_addr)
+
+chunks = []
+while True:
+    data = client_sock.recv(2048)
+    if not data:
+        break
+    chunks.append(data)
+
+client_sock.sendall(b''.join(chunks))
+client_sock.close()
